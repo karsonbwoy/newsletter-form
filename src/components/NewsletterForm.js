d026213806b958e3789643a522ebb1d1c674be8e
Sebastian <skarapyta@gmail.com>
2023-09-21 13:42:12 +0200

    Create newsletter component file

---
+++ src/components/NewsletterForm.js
@@ -0,0 +1,14 @@
+export default function NewsletterForm() {
+    return (
+        <div className="newsletter-container">
+            <div className="illustration"></div>
+            <h1>Stay Updated!</h1>
+            <p></p>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
